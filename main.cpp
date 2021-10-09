36c17c705516411f469c5fa9bcfead5d52c18a96
Rithak001734 <91362025+Rithak001734@users.noreply.github.com>
2021-10-09 06:00:02 -0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   cout<<"Rithak"<<endl<<"student id 20011212"<<endl<<"section j"<<endl;
+
+    char c;
+    cout<<"enter a character:"<<endl;
+    cin>>c;
+    cout<<"ASCCI value of"<<c<<"is"<<int(c)<<endl;
+    return 0;
+}
