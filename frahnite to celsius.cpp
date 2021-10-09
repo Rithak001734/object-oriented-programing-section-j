918019f487bd40160bc031efd4b0d2fd77d5da61
Rithak001734 <91362025+Rithak001734@users.noreply.github.com>
2021-10-09 06:05:40 -0100

    Add files via upload

---
+++ frahnite to celsius.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Rithak"<<endl<<"student id 20011212"<<endl<<"section j"<<endl;
+    float frahnite, celsius;
+    cout<<"Enter temperature in frahnite"<<endl;
+    cin>>frahnite;
+    celsius = (frahnite - 32)*5/9;
+    cout<<"The temperature in celsius is"<<celsius<<endl;
+    return 0;
+}
