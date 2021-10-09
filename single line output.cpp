7c159e7364830f0977457f60316ecc0342241ecd
Rithak001734 <91362025+Rithak001734@users.noreply.github.com>
2021-10-09 06:07:35 -0100

    Add files via upload

---
+++ single line output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     cout<<"Rithak"<<endl<<"student id 20011212"<<endl<<"section j"<<endl;
+     int maths = 80, physis = 77, chemistry = 68;
+     cout<<"maths"<<80<<endl<<"physis"<<77<<endl<<"chemistry"<<68<<endl;
+    return 0;
+}
